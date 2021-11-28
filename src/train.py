f27ff9a578ee63e8e1555484e6abb6f081e6f40a
unknown <7marianaramos@gmail.com>
2021-11-28 19:38:17 +0000

    svm, grid search new parameters, SMOTE, rfecv feature selector

--- src/train.py
+++ src/train.py
@@ -1,4 +1,6 @@
 import sys
+
+from matplotlib.pyplot import cla
 import joblib
 import pandas as pd
 from sklearn import metrics
@@ -7,15 +9,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import GridSearchCV, StratifiedKFold, KFold
 from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.svm import SVC
+from sklearn.feature_selection import RFECV
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
-from sklearn.feature_selection import f_classif
+from sklearn.feature_selection import f_classif, mutual_info_classif, f_regression, mutual_info_regression, SelectPercentile, SelectFpr, SelectFdr, SelectFwe, GenericUnivariateSelect
 from sklearn.feature_selection import mutual_info_classif
 from sklearn.feature_selection import f_regression
 from sklearn.feature_selection import mutual_info_regression
+from sklearn.pipeline import Pipeline
 import pickle
+from imblearn.over_sampling import SMOTE
 
 from pathlib import Path
 
@@ -30,8 +37,13 @@ def grid_search(classifier_name, submission_name):
     X = df.drop(columns=['loan_status'])
     y = df['loan_status']
     
+    # X, y = select_features(X, y)
+    # X , y = select_features_RFECV(X,y,classifier_name)
+    
     params = get_grid_params(classifier_name)
+
     classifier = get_classifier(classifier_name)
+
     grid_search_var = GridSearchCV(
         estimator=classifier,
         param_grid = params,
@@ -41,6 +53,7 @@ def grid_search(classifier_name, submission_name):
 
     grid_results = grid_search_var.fit(X, y)
 
+
     print('Best Parameters: ', grid_results.best_params_)
     print('Best Score: ', grid_results.best_score_)
     
@@ -49,6 +62,7 @@ def select_features(X, y):
     models_folder = Path("models/")
 
     bestfeatures = SelectKBest(score_func=f_classif, k=10) # f_classif, f_regression
+
     fit = bestfeatures.fit(X,y)
     dfscores = pd.DataFrame(fit.scores_)
     dfcolumns = pd.DataFrame(X.columns)
@@ -66,19 +80,44 @@ def select_features(X, y):
 
     return X, y
 
+def select_features_RFECV(X,y,classifier_name):
+    models_folder = Path("models/")
+    classifier=get_classifier(classifier_name)
+    bestfeatures = RFECV(classifier,scoring='roc_auc') 
+    fit = bestfeatures.fit(X,y)
+    dfscores = pd.DataFrame(fit.ranking_ )
+    dfcolumns = pd.DataFrame(X.columns)
+    featureScores = pd.concat([dfcolumns,dfscores],axis=1)
+    featureScores.columns = ['Specs','Score']  #naming the dataframe columns
+    print(featureScores.nlargest(10, 'Score'))  #print 10 best features
+    print(featureScores.nlargest(10,'Score')['Specs'].values.tolist())
+    best_attributes = featureScores.nlargest(10,'Score')['Specs'].values.tolist()
+    pickle.dump(best_attributes, open(models_folder/'attributes.pkl', "wb"))
+    X = X[best_attributes]
+
+    return X, y
+
+
 def train(classifier_name, submission_name):
     df = pd.read_csv('clean_data/' + submission_name + '-train.csv', delimiter=",", low_memory=False)
 
     X = df.drop(columns=['loan_status'])
     y = df['loan_status']
 
-    print(X.columns)
+    # Using SMOTE:
+    oversample = SMOTE()
+    X, y = oversample.fit_resample(X, y)
 
+    # SelectKBest Feature Selector
     X, y = select_features(X, y)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y, random_state=RS)
+    # RFECV Feature Selector 
+    #X,y = select_features_RFECV(X,y,classifier_name)
+    
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, stratify=y, random_state=RS)
 
-    classifier = get_classifier_best(classifier_name)
+    classifier = get_classifier_best(classifier_name) 
     classifier.fit(X_train, y_train)
 
     print("Performance on the training set")
@@ -107,6 +146,9 @@ def get_classifier(classifier):
         return RandomForestClassifier(random_state=RS)
     elif classifier == 'gradient_boosting':
         return GradientBoostingClassifier(random_state=RS)
+    elif classifier == 'svm':
+        return SVC(random_state=RS)
+
 
 def get_grid_params(classifier):
     if classifier == 'logistic_regression':
@@ -135,15 +177,34 @@ def get_grid_params(classifier):
             'min_samples_split':  [4, 6, 8],
             'min_samples_leaf':  [2, 4, 6]}
 
+    elif classifier == 'svm':
+        return {'C': [1, 10], 
+          'gamma': [0.001, 0.01, 1,'scale','auto'],
+          'kernel':['linear', 'poly', 'rbf', 'sigmoid'],
+          'degree':[1,2,3,4,5,6,7],
+          'coef0':[0.0, 0.1, 0.3, 0.5, 0.7],
+          'max_iter':[1,2,3,5,7,10],
+          'decision_function_shape':['ovo','ovr'],
+          'class_weight':[None, 'balanced', dict]}
+
+    #elif classifier == 'SelectKBest':
+    #    return {'score_func': [ mutual_info_classif, chi2, f_regression, mutual_info_regression, SelectPercentile, SelectFpr, SelectFdr, SelectFwe, GenericUnivariateSelect, f_classif]}
+
 
 def get_classifier_best(classifier):
     if classifier == 'logistic_regression':
+        # SelectKBest:
         return LogisticRegression(C = 0.01, class_weight= 'balanced', penalty= 'none', solver= 'saga')
+        # RFECV:
+        # return LogisticRegression(C = 0.01, class_weight= None, penalty= 'l2', solver= 'newton-cg')
     elif classifier == 'random_forest':
-        return RandomForestClassifier(class_weight= 'balanced_subsample', criterion= 'entropy', max_depth= 10, max_features= 'auto', min_samples_leaf= 6, min_samples_split= 2, n_estimators= 6)
+        return RandomForestClassifier(class_weight= 'balanced_subsample', criterion= 'gini', max_depth= 14, max_features= 'auto', min_samples_leaf= 4, min_samples_split= 2, n_estimators= 10)
+        # Juliane:
+        # return RandomForestClassifier(n_estimators=100, max_depth=30,class_weight= 'balanced') 
     elif classifier == 'gradient_boosting':
-        return GradientBoostingClassifier(criterion='friedman_mse', learning_rate=0.7, loss= 'exponential', min_samples_leaf= 2, min_samples_split= 8, n_estimators= 8)
-
+        return GradientBoostingClassifier(criterion='friedman_mse', learning_rate=0.7, loss= 'exponential', min_samples_leaf= 6, min_samples_split= 4, n_estimators= 12)
+    elif classifier == 'svm':
+        return SVC(C= 1, class_weight= 'balanced', coef0= 0.0, decision_function_shape= 'ovo', degree= 5, gamma= 'scale', kernel= 'poly', max_iter= 3, probability=True)
 
 def auc_scorer(y_true, y_pred):
     '''Scorer of Area Under Curve value'''
